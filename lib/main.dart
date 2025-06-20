a13e7559004b1f71e1229677317940925c1b45cf
Andrew Brogdon <brogdon@gmail.com>
2025-06-20 16:36:58 -0700

    Working with poetry game

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ai/firebase_ai.dart';
+import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -12,9 +18,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Widgets, yo'),
     );
   }
 }
@@ -29,12 +35,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late final GenerativeModel model;
+  late final ChatSession chat;
+  String chatHistory = '';
+  bool chatStarted = false;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    model = FirebaseAI.googleAI().generativeModel(
+      model: 'gemini-2.5-flash',
+      systemInstruction: Content.text('''
+        I'd like to play a poetry guessing game with you. Choose a famous poet,
+        and give me their name, plus the name of one of their poems and two
+        other famous poems they didn't write. I'll respond by picking one of the
+        poem names, and you tell me if it was written by the poet you chose.
+
+        After each round, don't wait for me to say I want to continue. Just
+        start a new round.
+      '''),
+    );
+
+    chat = model.startChat();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!chatStarted) {
+      chatStarted = true;
+      chat
+          .sendMessage(Content.text('I am ready to play!'))
+          .then(
+            (response) =>
+                setState(() => chatHistory = '${response.text ?? ''}\n\n'),
+          );
+    }
+  }
+
+  Future<void> sendMessage(String msg) async {
+    setState(() => chatHistory = '$chatHistory$msg\n\n');
+    final response = await chat.sendMessage(Content.text(msg));
+    setState(() => chatHistory = '$chatHistory${response.text}\n\n');
   }
 
   @override
@@ -44,22 +88,46 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: SizedBox.expand(
+          child: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 8, 8, 100),
+                  child: Text(chatHistory),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: textController,
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      ElevatedButton(
+                        onPressed: () async {
+                          final text = textController.text;
+                          textController.clear();
+                          await sendMessage(text);
+                        },
+                        child: Text('Clicky'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
